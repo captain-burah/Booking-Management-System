9511fbc97bfc4268e61e15b660f642a561128c39
captain_burah <cptburah@gmail.com>
2021-05-22 20:33:19 +0530

    created BookingFactory and DatabaseSeeder call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(BookablesTableSeeder::class);
          $this->call(CustomersTableSeeder::class);
+         $this->call(BookingsTableSeeder::class);
     }
 }
