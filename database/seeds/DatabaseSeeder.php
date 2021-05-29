f1be842e7371a4d1fadc2440884a4153cbfd8c46
captain_burah <cptburah@gmail.com>
2021-05-29 17:26:39 +0530

    ReviewList | model Factory & Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(BookablesTableSeeder::class);
          $this->call(CustomersTableSeeder::class);
          $this->call(BookingsTableSeeder::class);
+         $this->call(ReviewsTableSeeder::class);
     }
 }
