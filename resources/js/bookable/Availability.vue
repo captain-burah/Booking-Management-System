6cbf142d4c1c7cb23ad5f21eaf798e9c2aa0e30f
captain_burah <cptburah@gmail.com>
2021-05-30 13:01:22 +0530

    ReviewList | Reading data in Front-end, redefined new props for .params.id

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -59,6 +59,9 @@
 
 <script>
 export default {
+    props: {
+        bookableId: String
+    },
     data() {
         return {
             from: null,
@@ -74,7 +77,7 @@ export default {
             this.loading = true;
             this.errors = null;
             axios
-                .get(`/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`)
+                .get(`/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`)
                 .then(response => {
                     this.status = response.status;
                 })
