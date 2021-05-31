7c185e2a8873b45cc1c633875b409e546dbd9954
captain_burah <cptburah@gmail.com>
2021-05-31 07:04:51 +0530

    ReviewList | Configured fontawesome & StarRating components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import VueRouter from "vue-router";
 import Vue from "vue";
 import Index from "./Index";
 import moment from "moment";
+import StarRating from "./shared/components/StarRating";
+
 
 window.Vue = require('vue');
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
@@ -12,7 +14,9 @@ window.Vue = require('vue');
 
 Vue.use(VueRouter);
 
-Vue.filter("fromNow", value => moment(value).fromNow())
+Vue.filter("fromNow", value => moment(value).fromNow());
+
+Vue.component("star-rating", StarRating);
 
 const app = new Vue({
     el: '#app',
---
+++ resources/js/shared/components/StarRating.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="d-flex">
+        <i class="fas fa-star" v-for="star in fullStar" :key="'full' + star"></i>
+        <i class="fas fa-star-half-alt" v-if="halfStar"></i>
+        <i class="far fa-star" v-for="star in emptyStar" :key="'empty' + star"></i>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        rating: Number
+    },
+    computed: {
+        halfStar() {
+            return true;
+        },
+        fullStar() {
+            return 3;
+        },
+        emptyStar() {
+            return 1;
+        },
+    }
+}
+</script>
